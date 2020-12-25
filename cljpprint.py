17bc06a1b70c598ad88bd2212bb0a2fb3fcb5a63
Roman Bataev <rbataev@walmartlabs.com>
2020-12-25 11:03:11 -0800

    Revert "Merge pull request #2 from pedrorgirardi/format-selected-text"
    This reverts commit e22c599e6ea526d56cec36ed99a2819791c0bf91, reversing
    changes made to a3149aefd63f0eabaf0f097ab7a104ce329ffc2a.

--- cljpprint.py
+++ cljpprint.py
@@ -164,12 +164,7 @@ class CljpprintCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         if not is_clojure_source(self.view):
             return
-
-        # Format selected text, or the whole buffer in case no text is selected.
-        regions = list(filter(lambda r: r.begin() != r.end(), self.view.sel()))
-        regions = regions if regions else [sublime.Region(0, self.view.size())]
-
-        run_formatter(edit, self.view, regions)
+        run_formatter(edit, self.view, [sublime.Region(0, self.view.size())])
 
 
 class CljPpprintListener(sublime_plugin.EventListener):
